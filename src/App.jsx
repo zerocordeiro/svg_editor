ed0845a993a3a5434ecb18bc66984ceee8954d7a
zerocordeiro <juseh.r@gmail.com>
2026-07-10 15:53:26 +0100

    Turning the SVG into an array is getting better, but still a very "manual" job. - need a way to more easily point to each element of the SVG

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,126 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // we need an object in which to store the SVG file
+  const [svgFile, setSvgFile] = useState(null);
+  // we also need an object that will store the svg structure, as in an object that will represent the svg structure, so we can manipulate it later on.
+  const [svgStructure, setSvgStructure] = useState([]);
+  const [svgDoc, setSvgDoc] = useState(null);
+
+  async function handleFileChange(event) {
+    const svgView = document.getElementById('svgView');
+    // this will check the uploaded file and first see if it is a SVG. If it isn't, it will alert the user that the file is not a SVG and end the execution. If it is a SVG, it will alert the user that the file has been changed.
+    const file = event.target.files[0];
+    if (file && file.type === 'image/svg+xml') {
+      // alert('File changed');
+    } else {
+      alert('The selected file is not a SVG.');
+      return;
+    }
+    console.log(file);
+    // after checking that the file is a svg, it will read the file and parse it as an object. It will then log the object to the console.
+    const reader = new FileReader();
+    reader.onload = async function(e) {
+      await readSVG(e);
+    };
+    async function readSVG(e) {
+      const svgString = e.target.result;
+      const parser = new DOMParser();
+      const svgParsed = parser.parseFromString(svgString, 'image/svg+xml');
+
+      console.log('svg parsed: ', svgParsed);
+
+      let groups = [];
+      let elements = [];
+
+      const svgStyle = svgParsed.querySelector('style');
+      console.log('svg style: ', svgStyle);
+
+      // We'll make a function that will be used recursively to check the elements inside other elements in the SVG. For example, it will be used to check insige a group and if this group has anopther group as a child it will execute the same function to see inside this child group. The data will be organized as an object with the parameters: tag, id, classList, contents (Array of objects with the same structure).
+      function traverseElements(element) {
+        const elementData = {};
+        elementData.tag = element.tagName;
+        elementData.id = element.id ? element.id : 'noId';
+        elementData.classList = Array.from(element.classList);
+        elementData.children = [];
+        Array.from(element.children).forEach(child => {
+          if (child.tagName === 'g') {
+            console.log('group found: ', child);
+            elementData.children.push(traverseElements(child)); // Recursively traverse child elements
+          } else {
+            console.log('element found: ', child);
+            elementData.children.push({
+              tag: child.tagName,
+              id: child.id ? child.id : 'noId',
+              classList: Array.from(child.classList),
+              children: []
+            });
+          }
+        });
+        return elementData;
+      }
+
+      let svgStructureArray = [];
+
+      // Traverse through all SVG elements
+        Array.from(svgParsed.querySelectorAll('svg > *')).forEach(element => {
+            if (element.tagName === 'g') { // Groups are <g> tags
+              console.log('group found: ', element);
+                groups.push({
+                    id: element.id,
+                    attributes: Object.keys(element.attributes)
+                });
+            } else{
+              console.log('element found: ', element);
+              elements.push({
+                  tag: element.tagName,
+                  id: element.id,
+                  attributes: Object.keys(element.attributes)
+              });
+            }
+          svgStructureArray.push(traverseElements(element));
+
+        });
+
+        // You can now work with the groups and elements arrays
+        console.log('groups: ',groups);
+        console.log('elements: ',elements);
+        
+        console.log('svg structure: ', svgStructureArray); // This will now show the correct structure
+        setSvgStructure(svgStructureArray); // Set it once with the complete array
+
+      setSvgDoc(svgParsed);
+      console.log('svg contents:', svgParsed);
+      svgView.innerHTML = svgParsed.documentElement.outerHTML;
+      
+      return false;
+    }
+    reader.readAsText(file);
+  }
+
+  
 
   return (
     <>
-      <section id="left">
-        <div className="elementList">
-          Element List
+      <section id="appContainer">
+        <div className="flexrow fullwidth flex1">
+          <div id="elementList">
+            Element List
+            <span id="svgContents"></span>
+          </div>
+          <div id="SVGDisplay">
+            Display
+            {/* I will insert an input which will be expecting a svg. It will get this SVG, then read it and parse it as an object */}
+            <input id="svgInput" type="file" accept=".svg" onChange={handleFileChange} />
+            <span id="svgView"></span>
+          </div>
+          <div id="propertiesList">
+            Properties List
+          </div>
         </div>
-        <div>
-          
-        </div>
+        {/* <div id="timelineContainer">
+          Timeline Container
+        </div> */}
       </section>
     </>
   )
