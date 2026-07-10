6f259e255eb54f40b9e0e38f99c514739264dd75
zerocordeiro <juseh.r@gmail.com>
2026-07-10 18:19:34 +0100

    Now it is creating an unique ID whenever the element doesn't have one. But it still needs to actually add the ID to the SVG that is being shown in the page. We need a way to either edit the SVG in the page or create a new SVG with the data after it is read and some modifications are made.
    It is possible to add ID to an element via JS, but we need to find a way to point to the desired element while we're reading the SVG to then assign the respective ID to it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import { useState } from 'react'
 import './App.css'
 
+class svgController {
+  // class that will be used to make an individual controller for each element and group of the SVG. This class will carry methods that will be used to edit properties of the elements and groups, as well as methods to add and remove elements and groups. It will also have a method to get the properties of the element or group.
+  constructor(element, elId) {
+
+    this.element = element;
+    this.button = document.createElement('button');
+    this.element.id = elId;
+    this.button.innerHTML = `${this.element.tagName} - ID: ${elId ? elId : 'noId'}`;
+    this.button.addEventListener('click', () => {
+      this.changeStroke();
+    });
+    this.init();
+  }
+  init() {
+    // Initialization code here
+    document.querySelector('#svgContents').appendChild(this.button);
+    // this.changeStroke();
+  }
+  changeStroke(){
+    console.log('changing stroke of element: ', this.element);
+    this.element = document.getElementById(`${this.element.id}`);
+    this.element.setAttribute('stroke', 'red');
+    this.element.setAttribute('stroke-width', '2px');
+  }
+}
+
 function App() {
   // we need an object in which to store the SVG file
   const [svgFile, setSvgFile] = useState(null);
   // we also need an object that will store the svg structure, as in an object that will represent the svg structure, so we can manipulate it later on.
   const [svgStructure, setSvgStructure] = useState([]);
   const [svgDoc, setSvgDoc] = useState(null);
-
+  
   async function handleFileChange(event) {
     const svgView = document.getElementById('svgView');
     // this will check the uploaded file and first see if it is a SVG. If it isn't, it will alert the user that the file is not a SVG and end the execution. If it is a SVG, it will alert the user that the file has been changed.
@@ -38,26 +64,31 @@ function App() {
       console.log('svg style: ', svgStyle);
 
       // We'll make a function that will be used recursively to check the elements inside other elements in the SVG. For example, it will be used to check insige a group and if this group has anopther group as a child it will execute the same function to see inside this child group. The data will be organized as an object with the parameters: tag, id, classList, contents (Array of objects with the same structure).
-      function traverseElements(element) {
+      async function traverseElements(element) {
         const elementData = {};
+        const currentDate = await new Date().getTime();
+        
         elementData.tag = element.tagName;
-        elementData.id = element.id ? element.id : 'noId';
+        elementData.id = element.id ? element.id : currentDate;
         elementData.classList = Array.from(element.classList);
         elementData.children = [];
         Array.from(element.children).forEach(child => {
           if (child.tagName === 'g') {
             console.log('group found: ', child);
             elementData.children.push(traverseElements(child)); // Recursively traverse child elements
+            
           } else {
             console.log('element found: ', child);
             elementData.children.push({
               tag: child.tagName,
-              id: child.id ? child.id : 'noId',
+              id: child.id ? child.id : currentDate,
               classList: Array.from(child.classList),
               children: []
             });
+            // document.querySelector('#svgContentsList').innerHTML += `<li>Element: ${child.tagName}, ID: ${child.id ? child.id : 'noId'}, Classes: ${Array.from(child.classList).join(', ')}</li>`;
           }
         });
+        const myController = new svgController(element,elementData.id);
         return elementData;
       }
 
@@ -107,7 +138,9 @@ function App() {
         <div className="flexrow fullwidth flex1">
           <div id="elementList">
             Element List
-            <span id="svgContents"></span>
+            <span id="svgContents">
+
+            </span>
           </div>
           <div id="SVGDisplay">
             Display
